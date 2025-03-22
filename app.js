eeb5f0e68789e31428d6a69a954a2a73f1b21f9f
bigonil <luca.bigoni@gmail.com>
2025-03-22 19:49:01 +0100

    Update Hello World! Message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello world, I’m diving into the coding universe and this is my first step!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
