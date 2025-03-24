80fe0f448de27f8402133a99912d4b54280ab0a3
bigonil <luca.bigoni@gmail.com>
2025-03-24 12:27:10 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello world, I’m diving into the coding universe and this is my first step!'));
+app.get('/', (req, res) => res.send('Full pipeline test, Hello world, I’m diving into the coding universe and this is my first step!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
